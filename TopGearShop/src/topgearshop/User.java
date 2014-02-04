da2a6667af0a93276fb0e7add2fcb6da2b1d11bf
klkita <kleon.kita@oracle.com>
2014-02-04 12:25:02 -0500

    Adding User Managers

---
+++ TopGearShop/src/topgearshop/User.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topgearshop;
+
+/**
+ *
+ * @author klkita
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    
+    public User(String f, String l, String e, String p){
+        this.firstName = f;
+        this.lastName = l;
+        this.email = e;
+        this.phone = p;
+    }
+    
+}
